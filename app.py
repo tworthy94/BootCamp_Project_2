a964e993145b2712a5c0b10689d7ac410023fc47
Nicholas Worthy <40613355+tworthy94@users.noreply.github.com>
2019-07-13 13:30:32 -0400

    Flask app set up, beginnings of website started

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
-from flask import Flask
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request)
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/Volcano_Data'
+app.config['DEBUG'] = True
 db = SQLAlchemy(app)
+db_uri = 'postgresql://postgres:postgres@localhost/Volcano_Data'
+engine = create_engine(db_uri)
+
+@app.route("/")
+def index():
+     return render_template("index.html")
+
+
+@app.route("/data")
+def db_push():
+    query = engine.execute('SELECT * FROM ' '"volcano_data"')
+    full_db = query.fetchall()
+    return render_template("raw_data.html", data=full_db)
+
+@app.route("/dashboards")
+def dashboard():
+    return render_template("dashboards.html")
+
+if __name__ == "__main__":
+    app.run()
